cf19cb610b9533c13a1113000f9ec6f6a2a7d9d6
vlmna13 <vlmna13@gmail.com>
2024-12-20 16:14:47 +0300

    feat: add dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  let firstsLetters = [];
+  members = members.filter((member) => {
+    return typeof member === "string";
+  });
+  members.forEach((name) => {
+    firstsLetters.push(name.trim()[0].toUpperCase());
+  });
+  return firstsLetters.sort().join('');
 }
 
 module.exports = {
